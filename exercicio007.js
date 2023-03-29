e57cbecfd52d5b1af7ffbde2de6dc33eef16e71b
Kaique Nunes <kaiquenlp@gmail.com>
2023-03-29 17:25:43 -0300

    Initial commit

---
+++ exercicio007.js
@@ -0,0 +1,60 @@
+
+var  receiverORdonor = ('1')
+var  userBloods = ('A')
+
+
+if (receiverORdonor == 1 && userBloods.equals(Blood1)) {
+    
+    System.out.println("This type of blood can receive from:" +Blood1 + "," +Blood5 + "," +Blood2 + "," + Blood6 +  ".");
+
+}else if (receiverORdonor == 2 && userBloods.equals(Blood1)) {
+System.out.println("This type of blood can donor to:" +Blood1 + "," +Blood4 + ".");
+
+}
+
+if (receiverORdonor == 1 && userBloods.equals(Blood2)) {
+System.out.println("This type of blood can receive from: " +Blood2 + "," +Blood6 + ".");
+}else if (receiverORdonor == 2 && userBloods.equals(Blood2)) {
+System.out.println("This type of blood can donor to:" +Blood2 + "," +Blood1 + "," +Blood3 + "," +Blood4 + ".");
+
+}
+
+if (receiverORdonor == 1 && userBloods.equals(Blood3)) {
+System.out.println("This type of blood can receive from: " +Blood3 + "," +Blood7 + "," +Blood2 + "," + Blood6 +  ".");
+
+}else if (receiverORdonor == 2 && userBloods.equals(Blood3)) {
+System.out.println("This type of blood can donor to:" +Blood3 + "," +Blood4 + ".");
+}
+
+if (receiverORdonor == 1 && userBloods.equals(Blood4)) {
+System.out.println("This type of blood can receive from: every type of Blood.");
+
+}else if (receiverORdonor == 2 && userBloods.equals(Blood4)) {
+System.out.println("This type of blood can donor to:" +Blood4 + ".");
+}
+
+if (receiverORdonor == 1 && userBloods.equals(Blood5)) {
+System.out.println("This type of blood can receive from:" +Blood5 + "," +Blood6 + ".");
+
+}else if (receiverORdonor == 2 && userBloods.equals(Blood5)) {
+System.out.println("This type of blood can donor to:" +Blood1 + "," +Blood5 + "," +Blood4 + "," +Blood8 + ".");
+}
+
+if (receiverORdonor == 1 && userBloods.equals(Blood6)) {
+System.out.println("This type of blood can receive from:" +Blood6 + ".");
+}else if (receiverORdonor == 2 && userBloods.equals(Blood6)) {
+System.out.println("This type of blood can donor to: every type of blood.");
+}
+
+if (receiverORdonor == 1 && userBloods.equals(Blood7)) {
+System.out.println("This type of blood can receive from:" +Blood7 + "," +Blood6 + ".");
+}else if (receiverORdonor == 2 && userBloods.equals(Blood7)) {
+System.out.println("This type of blood can donor to:" +Blood3 + "," +Blood7 + "," +Blood4 + "," +Blood8 + ".");
+}
+
+if (receiverORdonor == 1 && userBloods.equals(Blood8)) {
+System.out.println("This type of blood can receive from:" +Blood8 + "," +Blood5 + "," +Blood7 + "," + Blood6 +  ".");
+}else if (receiverORdonor == 2 && userBloods.equals(Blood8)) {
+System.out.println("This type of blood can donor to:" +Blood4 + "," +Blood8 + ".");
+}
+
